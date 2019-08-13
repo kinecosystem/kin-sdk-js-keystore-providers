799726ac672583922da862a2df268ea66d442bfb
Aryeh Katz <in@akatz.io>
2019-08-13 14:51:13 +0300

    remove unused webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   entry: {
