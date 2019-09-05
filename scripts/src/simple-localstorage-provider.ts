361eee6b54800bd3c808da1682afa8223a8f0586
Aryeh Katz <in@akatz.io>
2019-09-05 15:28:16 +0300

    fix naming

--- scripts/src/simple-localstorage-provider.ts
+++ scripts/src/simple-localstorage-provider.ts
@@ -3,13 +3,13 @@ import { LocalStorageHandler } from "./libs/local-storage-handler";
 
 declare global {
   interface Window {
-    KeystoreProvider: any
+    SimpleLocalStorageKeystoreProvider: typeof SimpleLocalStorageKeystoreProvider;
   }
 }
 
-const KIN_WALLET_STORAGE_INDEX = 'kin-wallet';
+const KIN_WALLET_STORAGE_INDEX = "kin-wallet";
 
-export class LocalStorageKeystoreProvider implements KinSdk.KeystoreProvider {
+export class SimpleLocalStorageKeystoreProvider implements KinSdk.KeystoreProvider {
   private _storage: LocalStorageHandler;
 
   constructor(private readonly _sdk: typeof KinSdk, secret: string) {
@@ -17,33 +17,37 @@ export class LocalStorageKeystoreProvider implements KinSdk.KeystoreProvider {
   }
 
   public addKeyPair(seed: string) {
-    if (seed == undefined)
-      this._storage.add(this._sdk.KeyPair.generate().seed);
-    else
-      this._storage.add(seed);
+    if (seed == undefined) this._storage.add(this._sdk.KeyPair.generate().seed);
+    else this._storage.add(seed);
   }
 
   get accounts() {
-    let seeds = this._storage.get()
-    let accounts = seeds.map(seed => this._sdk.KeyPair.fromSeed(seed).publicAddress)
-    return Promise.resolve(accounts)
+    let seeds = this._storage.get();
+    let accounts = seeds.map(seed => this._sdk.KeyPair.fromSeed(seed).publicAddress);
+    return Promise.resolve(accounts);
   }
 
   public async sign(accountAddress: string, transactionEnvelpoe: string) {
     let seeds = await this._storage.get();
     const seed = seeds.find(seed => {
       let tmpAcc = this._sdk.KeyPair.fromSeed(seed);
-      if (accountAddress == tmpAcc.publicAddress)
-        return seed;
+      if (accountAddress == tmpAcc.publicAddress) return seed;
     });
     if (seed != null) {
       const tx = new this._sdk.XdrTransaction(transactionEnvelpoe);
       const signers = new Array();
       signers.push(this._sdk.BaseKeyPair.fromSecret(seed));
       tx.sign(...signers);
-      return Promise.resolve(tx.toEnvelope().toXDR("base64").toString());
-    } else { return Promise.reject("keypair null"); }
+      return Promise.resolve(
+        tx
+          .toEnvelope()
+          .toXDR("base64")
+          .toString()
+      );
+    } else {
+      return Promise.reject("keypair null");
+    }
   }
 }
 
-window.KeystoreProvider = LocalStorageKeystoreProvider
+window.SimpleLocalStorageKeystoreProvider = SimpleLocalStorageKeystoreProvider;
