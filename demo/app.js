34b6516f418406bdb00b59d351ebd79949167282
Aryeh Katz <in@akatz.io>
2019-09-09 12:46:58 +0300

    refactoring

--- demo/app.js
+++ demo/app.js
@@ -1,7 +1,6 @@
 const SEED = "SARVSAHEAIHNN3WTCUX2YPNHPXB2PUDL3UBK3Z4RZMNPXDNUG5QD5VFF";
 const PK = "GCEMQWMFAFO5UOZHNVFQTAGQG7F3KAVNNA5TWIMO7XPJIRGU4WR3NHQJ";
 const RECIVER_PK = "GD5LWUF54MHZRZHFYFCHZ53TUVT3TWIGDM2JNRJDDWKBD4EZGQPEFULF";
-const COOKIE_MAX_LIFE = 3600 * 24; // 24h
 
 const keyStoreProvider = new ExtensionKeystoreProvider(KinSdk);
 const kinClient = new KinSdk.KinClient(KinSdk.Environment.Testnet, keyStoreProvider);
@@ -10,80 +9,65 @@ Vue.config.productionTip = false;
 new Vue({
   el: "#root",
   data: {
-    accounts: []
+    accounts: [],
+    properties: {
+      extension_auto_install: true
+    }
   },
   created: function() {
     this.refreshLocalStorageAccounts();
   },
   methods: {
-    installExtension: function() {
-      const cookie = `kin_ext_install_source='${window.location.hostname}';`;
-      const maxAge = `max-age=${COOKIE_MAX_LIFE};`;
-      const path = "path=/;";
-      document.cookie = cookie.concat(maxAge, path);
-      if (this._properties.extension_auto_install) {
-        window.open(EXTENSION_URL);
+    installExtension: async function() {
+      try {
+        const installExtension = await keyStoreProvider.isInstalled(this.properties);
+        console.info(`installExtension: ${installExtension}`);
+      } catch (err) {
+        console.warn(`installExtension: ${err}`);
       }
     },
-    refreshLocalStorageAccounts: function() {
-      console.log("app: get accounts");
-      kinClient.kinAccounts
-        .then(accounts => {
-          console.log("app: get accounts -> then");
-          this.accounts = accounts.map(account => {
-            account.balance = 0;
-            return account;
+    refreshLocalStorageAccounts: async function() {
+      try {
+        const accounts = await kinClient.kinAccounts;
+        this.accounts = accounts.map(account => {
+          account.balance = 0;
+          return account;
+        });
+        this.activateAccounts();
+        this.refreshBalances();
+      } catch (err) {
+        console.warn(`refreshLocalStorageAccounts: ${err}`);
+      }
+    },
+    tip: async function() {
+      console.log("client: tip clicked");
+      const txBuilder = await this.accounts[0].buildTransaction({
+        address: RECIVER_PK,
+        amount: 1,
+        fee: 100,
+        memoText: "Tip some kin"
+      });
+      console.log("client: tip -> builder");
+      await this.accounts[0].submitTransaction(txBuilder.toString());
+      console.log("client: tip -> refresh balance");
+      this.refreshBalances();
+    },
+    refreshBalances: async function() {
+      for (let account in this.accounts) {
+        const balance = await account.getBalance();
+        account.balance = balance;
+      }
+    },
+    activateAccounts: async function() {
+      for (let account in this.accounts) {
+        const isAccountExisting = await account.isAccountExisting();
+        if (!exists) {
+          kinClient.friendbot({
+            address: account._publicAddress,
+            amount: 1000
           });
-          this.activateAccounts();
-          this.refreshBalances();
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    },
-    tipSomeone: function() {
-      this.accounts[0]
-        .buildTransaction({
-          address: RECIVER_PK,
-          amount: 1,
-          fee: 100,
-          memoText: "Tip some kin"
-        })
-        .then(txBuilder => {
-          return this.accounts[0].submitTransaction(txBuilder.toString());
-        })
-        .then(txId => {
-          console.log(`tipped: ${txId}`);
-          this.refreshBalances();
-        });
-    },
-    createLocalStorageAccount: function() {
-      let isAlreadyLoaded = this.accounts.some(account => account._publicAddress == PK);
-      if (isAlreadyLoaded) {
-        console.log("account was already loaded");
-      } else {
-        keyStoreProvider._localStorageProvider.add(SEED);
-        this.refreshLocalStorageAccounts();
+        }
       }
-    },
-    refreshBalances: function() {
-      this.accounts.forEach(account => {
-        account.getBalance().then(balance => {
-          account.balance = balance;
-        });
-      });
-    },
-    activateAccounts: function() {
-      this.accounts.forEach(account => {
-        account.isAccountExisting().then(exists => {
-          if (!exists) {
-            kinClient.friendbot({
-              address: account._publicAddress,
-              amount: 1000
-            });
-          }
-        });
-      });
     }
   }
 });
--- scripts/src/extension-provider.ts
+++ scripts/src/extension-provider.ts
@@ -7,51 +7,63 @@ declare global {
 }
 
 const EXTENSION_ID = "ajibgglefmgckbegajkboddpjkokdkae";
+const MY_SECRET = "my secret";
+const COOKIE_MAX_LIFE = 3600 * 24; // 24h
 const EXTENSION_URL = `https://chrome.google.com/webstore/detail/kin-sdk-extension/${EXTENSION_ID}`;
 
-export interface ExtensionProperties {
+export interface ExtensionOptions {
   extension_auto_install: boolean;
+  debug: boolean;
 }
 
 export class ExtensionKeystoreProvider implements KinSdk.KeystoreProvider {
-  constructor(private readonly _sdk: typeof KinSdk) {}
+  private ready = false;
+  constructor(
+    private readonly _sdk: typeof KinSdk,
+    private readonly options: ExtensionOptions = {
+      extension_auto_install: true,
+      debug: process.env.NODE_ENV !== "production"
+    }
+  ) {
+    if (chrome.runtime && chrome.runtime.sendMessage) {
+      this.ready = true;
+    } else {
+      if (this.options.debug) console.warn("You are trying to use ExtensionKeystoreProvider but Kin Extension is missing.");
+    }
+  }
 
   get accounts(): Promise<string[]> {
-    console.log("extension-provider: get accounts");
-    return new Promise(resolve => {
-      chrome.runtime.sendMessage(EXTENSION_ID, { action: "accounts" }, (results: string[]) => {
-        console.log("extension-provider: accounts = " + results);
-        resolve(results);
-      });
+    return new Promise((resolve, reject) => {
+      if (this.ready)
+        chrome.runtime.sendMessage(EXTENSION_ID, { action: "ACCOUNTS", secret: MY_SECRET }, (results: string[] = []) => resolve(results));
     });
   }
 
   public sign(accountAddress: string, transactionEnvelpoe: string): Promise<string> {
     return new Promise(resolve => {
-      chrome.runtime.sendMessage(EXTENSION_ID, { action: "sign", data: { accountAddress, transactionEnvelpoe } }, (reply: string) => {
-        resolve(reply);
-      });
-    });
-  }
-
-  public isInstalled() {
-    return new Promise(resolve => {
-      chrome.runtime.sendMessage(EXTENSION_ID, { action: "version" }, reply => {
-        if (!reply) {
-          resolve(false);
-        } else {
-          resolve(true);
+      chrome.runtime.sendMessage(
+        EXTENSION_ID,
+        { action: "SIGN", secret: MY_SECRET, data: { accountAddress, transactionEnvelpoe } },
+        (signedTx: string) => {
+          resolve(signedTx);
         }
-      });
+      );
     });
   }
 
-  public async showSessionPopup() {
-    chrome.runtime.sendMessage(EXTENSION_ID, { action: "tip" }, reply => {
-      if (!reply) {
-        console.log("somthing is not working");
-      } else {
-        console.log("got a replay");
+  public isInstalled(properties: { extension_auto_install: boolean }) {
+    return new Promise((resolve, reject) => {
+      try {
+        chrome.runtime.sendMessage(EXTENSION_ID, { action: "IS_INSTALLED" }, reply => {
+          if (reply) resolve(true);
+        });
+      } catch (err) {
+        const cookie = `kin_ext_install_source='${window.location.hostname}';`;
+        const maxAge = `max-age=${COOKIE_MAX_LIFE};`;
+        const path = "path=/;";
+        document.cookie = cookie.concat(maxAge, path);
+        if (properties.extension_auto_install) window.open(EXTENSION_URL);
+        reject(false);
       }
     });
   }
